dbd90ecc31a018e53d04b9f7c0229ba5cc6e52ce
Дмитрий Горяйнов <goryaynov.dima@mail.ru>
2025-04-06 01:05:18 +0700

    refactoring user create func

--- bot/crud/__init__.py
+++ bot/crud/__init__.py
@@ -1,4 +1,1 @@
 from .users import create_new_user, get_user
-
-
-__all__ = ("create_new_user", "get_user",)
--- bot/crud/users.py
+++ bot/crud/users.py
@@ -1,22 +1,21 @@
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from app.models import User, ShoppingList
+from bot.models import User
+from bot.schemas.users import User as UserSchema
 
 
 async def create_new_user(
         session: AsyncSession, telegram_id: int) -> bool | None:
+    user_data = UserSchema(telegram_id=telegram_id)
     try:
         query = await session.execute(
-            select(User).filter_by(telegram_id=telegram_id)
+            select(User).filter_by(telegram_id=user_data.telegram_id)
         )
         user = query.scalar_one_or_none()
         if not user:
-            new_user = User(telegram_id=telegram_id)
+            new_user = User(**user_data.model_dump())
             session.add(new_user)
-            await session.flush()
-            new_user_shopping_list = ShoppingList(user_id=new_user.id)
-            session.add(new_user_shopping_list)
             await session.commit()
             return True
         return False
@@ -25,9 +24,9 @@ async def create_new_user(
         print(f"Error occurred while creating user: {e}")
 
 
-async def get_user(session: AsyncSession, telegram_id: int) -> User:
+async def get_user(session: AsyncSession, telegram_id: int) -> User | None:
     query = await session.execute(
-        select(User).filter_by(telegram_id=telegram_id)
+        select(User).filter_by(telegram_id=str(telegram_id))
     )
     user = query.scalar_one_or_none()
-    return user.telegram_id
+    return user
