2ed5fa7c7a40507137ae70366130968622742a0a
Дмитрий Горяйнов <goryaynov.dima@mail.ru>
2025-04-06 01:03:45 +0700

    fix env migrations file

--- migrations/env.py
+++ migrations/env.py
@@ -6,9 +6,9 @@ from sqlalchemy.engine import Connection
 from sqlalchemy.ext.asyncio import async_engine_from_config
 
 from alembic import context
-from app.core.database import Base
-from app.config import settings
-from app.models import *
+from bot.core.database import Base
+from bot.config import settings
+from bot.models import *
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
